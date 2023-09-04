34c92f5dd155e4a3a764f012d97a430402e578a3
AndrLang <hid4086@gmail.com>
2023-09-04 16:09:41 +0300

    init project

---
+++ app/main.py
@@ -0,0 +1,9 @@
+from fastapi import FastAPI
+
+from app.bookings.router import router as router_booking
+from app.users.router import router as router_users
+
+app = FastAPI()
+
+app.include_router(router_users)
+app.include_router(router_booking)
