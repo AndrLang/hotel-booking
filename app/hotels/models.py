6f33cc18dc53b9a7becbca24e065eaeb3382bab4
AndrLang <hid4086@gmail.com>
2023-09-10 21:00:32 +0300

    add routers, schemases, dao

--- app/hotels/models.py
+++ app/hotels/models.py
@@ -1,15 +1,16 @@
-from sqlalchemy import JSON
+from sqlalchemy import JSON, Integer
 from sqlalchemy.orm import Mapped, mapped_column
-
 from app.database import Base
 
 
 class Hotels(Base):
     __tablename__ = "hotels"
 
-    id: Mapped[int] = mapped_column(primary_key=True)
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
     name: Mapped[str]
     location: Mapped[str]
     services: Mapped[list[str]] = mapped_column(JSON)
     rooms_quantity: Mapped[int]
     image_id: Mapped[int]
+
+
--- app/main.py
+++ app/main.py
@@ -2,8 +2,16 @@ from fastapi import FastAPI
 
 from app.bookings.router import router as router_booking
 from app.users.router import router as router_users
+from app.hotels.router import router as router_hotels
+from app.hotels.rooms.router import router as router_rooms
+
 
 app = FastAPI()
 
 app.include_router(router_users)
+app.include_router(router_hotels)
+app.include_router(router_rooms)
 app.include_router(router_booking)
+
+
+
